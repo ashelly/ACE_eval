08e8c214bfc5fbbaca20cf15aa8b74042493354d
AShelly <3831873+ashelly@users.noreply.github.com>
2019-08-15 15:26:44 -0700

    Fix compiler warning in ace_eval.h

--- ace_eval.h
+++ ace_eval.h
@@ -1,10 +1,10 @@
 #include <stdint.h>
 #define Card uint32_t
 
+extern Card E(Card []);
 
 #define ACEHAND 5
 static inline Card ACE_makecard(int i){return 1<<(2*(i%13)+6)|1<<(i/13);}
 #define ACE_addcard(h,c)  h[c&7]+=c,h[3]|=c 
 #define ACE_evaluate(h)   E((h))
 #define ACE_rank(r)       ((r)>>28)
-
